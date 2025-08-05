f548735fc4b058de09d9777f08468d718b31ea29
Enrique Moreno <emorenocode@gmail.com>
2025-08-05 17:44:04 -0500

    Add DTOs for user registration and login, and update AuthController methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from 'src/auth/dto';
 import { NATS_SERVICE } from 'src/config';
 
 @Controller('auth')
@@ -7,13 +8,13 @@ export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {});
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto);
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {});
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto);
   }
 
   @Post('verify')
